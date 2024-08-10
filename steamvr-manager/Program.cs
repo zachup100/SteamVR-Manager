ca772505078e52124ab02ce665941825ddb74676
zachup100 <zachup100@users.noreply.github.com>
2024-08-10 15:13:34 -0400

    Update Program.cs
    Renamed Form1.cs to Main.cs
    Implementations for coloring windows titlebar

--- steamvr-manager/Program.cs
+++ steamvr-manager/Program.cs
@@ -8,10 +8,15 @@ namespace steamvr_manager
         [STAThread]
         static void Main()
         {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Main());
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
