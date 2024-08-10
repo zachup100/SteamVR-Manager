678c4560cfe10ae45acb221739f8be57351e5eea
zachup100 <zachup100@users.noreply.github.com>
2024-08-10 15:11:53 -0400

    Updated Main
    Renamed from Form1.cs
    First few steps towards implementing Theme system, still wonky.

---
+++ steamvr-manager/Main.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace steamvr_manager
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            steamvr_enabled = new CheckBox();
+            steamvr_directory = new TextBox();
+            steamvr_enabled_label = new Label();
+            select_directory = new Button();
+            topbar = new MenuStrip();
+            topbar_file_dropdown = new ToolStripMenuItem();
+            settings = new ToolStripMenuItem();
+            exit_menu_item = new ToolStripMenuItem();
+            topbar_help_dropdown = new ToolStripMenuItem();
+            check_for_updates = new ToolStripMenuItem();
+            about_menu_item = new ToolStripMenuItem();
+            steamvr_toggle = new Button();
+            topbar.SuspendLayout();
+            SuspendLayout();
+            // 
+            // steamvr_enabled
+            // 
+            steamvr_enabled.AutoCheck = false;
+            steamvr_enabled.AutoSize = true;
+            steamvr_enabled.Enabled = false;
+            steamvr_enabled.Location = new Point(11, 77);
+            steamvr_enabled.Name = "steamvr_enabled";
+            steamvr_enabled.Size = new Size(15, 14);
+            steamvr_enabled.TabIndex = 0;
+            steamvr_enabled.UseVisualStyleBackColor = true;
+            // 
+            // steamvr_directory
+            // 
+            steamvr_directory.Enabled = false;
+            steamvr_directory.Location = new Point(11, 43);
+            steamvr_directory.Name = "steamvr_directory";
+            steamvr_directory.Size = new Size(382, 23);
+            steamvr_directory.TabIndex = 1;
+            steamvr_directory.Text = "If you're seeing this, something went wrong";
+            steamvr_directory.WordWrap = false;
+            // 
+            // steamvr_enabled_label
+            // 
+            steamvr_enabled_label.AutoSize = true;
+            steamvr_enabled_label.Location = new Point(27, 76);
+            steamvr_enabled_label.Name = "steamvr_enabled_label";
+            steamvr_enabled_label.Size = new Size(99, 15);
+            steamvr_enabled_label.TabIndex = 2;
+            steamvr_enabled_label.Text = "SteamVR Enabled";
+            // 
+            // select_directory
+            // 
+            select_directory.Location = new Point(278, 72);
+            select_directory.Name = "select_directory";
+            select_directory.Size = new Size(115, 23);
+            select_directory.TabIndex = 3;
+            select_directory.Text = "Change Directory";
+            select_directory.UseVisualStyleBackColor = true;
+            select_directory.Click += select_directory_Click;
+            // 
+            // topbar
+            // 
+            topbar.BackColor = Color.FromArgb(238, 238, 238);
+            topbar.Items.AddRange(new ToolStripItem[] { topbar_file_dropdown, topbar_help_dropdown });
+            topbar.Location = new Point(0, 0);
+            topbar.Name = "topbar";
+            topbar.Size = new Size(405, 24);
+            topbar.TabIndex = 4;
+            topbar.Text = "menuStrip1";
+            // 
+            // topbar_file_dropdown
+            // 
+            topbar_file_dropdown.DropDownItems.AddRange(new ToolStripItem[] { settings, exit_menu_item });
+            topbar_file_dropdown.Name = "topbar_file_dropdown";
+            topbar_file_dropdown.Size = new Size(37, 20);
+            topbar_file_dropdown.Text = "File";
+            // 
+            // settings
+            // 
+            settings.Name = "settings";
+            settings.Size = new Size(180, 22);
+            settings.Text = "Settings";
+            settings.Click += settings_Click;
+            // 
+            // exit_menu_item
+            // 
+            exit_menu_item.Name = "exit_menu_item";
+            exit_menu_item.Size = new Size(180, 22);
+            exit_menu_item.Text = "Exit";
+            exit_menu_item.Click += exit_menu_item_Click;
+            // 
+            // topbar_help_dropdown
+            // 
+            topbar_help_dropdown.DropDownItems.AddRange(new ToolStripItem[] { check_for_updates, about_menu_item });
+            topbar_help_dropdown.Name = "topbar_help_dropdown";
+            topbar_help_dropdown.Size = new Size(44, 20);
+            topbar_help_dropdown.Text = "Help";
+            // 
+            // check_for_updates
+            // 
+            check_for_updates.Name = "check_for_updates";
+            check_for_updates.Size = new Size(180, 22);
+            check_for_updates.Text = "Check for updates";
+            check_for_updates.Click += check_for_updates_Click;
+            // 
+            // about_menu_item
+            // 
+            about_menu_item.Name = "about_menu_item";
+            about_menu_item.Size = new Size(180, 22);
+            about_menu_item.Text = "About";
+            about_menu_item.Click += about_menu_item_Click;
+            // 
+            // steamvr_toggle
+            // 
+            steamvr_toggle.Location = new Point(136, 104);
+            steamvr_toggle.Name = "steamvr_toggle";
+            steamvr_toggle.Size = new Size(132, 23);
+            steamvr_toggle.TabIndex = 5;
+            steamvr_toggle.Text = "Enable SteamVR";
+            steamvr_toggle.UseVisualStyleBackColor = true;
+            steamvr_toggle.Click += steamvr_toggle_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(405, 139);
+            Controls.Add(steamvr_toggle);
+            Controls.Add(select_directory);
+            Controls.Add(steamvr_enabled_label);
+            Controls.Add(steamvr_directory);
+            Controls.Add(steamvr_enabled);
+            Controls.Add(topbar);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = topbar;
+            MaximizeBox = false;
+            MaximumSize = new Size(421, 178);
+            MinimumSize = new Size(421, 178);
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SteamVR Manager";
+            topbar.ResumeLayout(false);
+            topbar.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox steamvr_enabled;
+        private TextBox steamvr_directory;
+        private Label steamvr_enabled_label;
+        private Button select_directory;
+        private MenuStrip topbar;
+        private ToolStripMenuItem topbar_help_dropdown;
+        private ToolStripMenuItem about_menu_item;
+        private ToolStripMenuItem topbar_file_dropdown;
+        private ToolStripMenuItem exit_menu_item;
+        private Button steamvr_toggle;
+        private ToolStripMenuItem check_for_updates;
+        private ToolStripMenuItem settings;
+    }
+}
---
+++ steamvr-manager/Main.cs
@@ -0,0 +1,151 @@
+using steamvr_manager;
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+using Newtonsoft.Json.Linq;
+using System.Xml;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+
+namespace steamvr_manager
+{
+    public partial class Main : Form
+    {
+
+        private Changelog ChangelogForm = new Changelog();
+        private Settings Settings = new Settings();
+        private Manager Manager = new Manager();
+        private About AboutForm = new About();
+        private Themes Themes = new Themes();
+
+        private bool Is64Bit = Environment.Is64BitOperatingSystem; // Placeholder for now
+        private void about_menu_item_Click(object sender, EventArgs e) { AboutForm.ShowDialog(); }
+        private void check_for_updates_Click(object sender, EventArgs e) { ChangelogForm.ShowDialog(); }
+        private void settings_Click(object sender, EventArgs e) { Settings.ShowDialog(); }
+        // Close the application, not sure what to put in that menu for now
+        private void exit_menu_item_Click(object sender, EventArgs e) { Application.Exit(); }
+
+        // TESTING STUFF
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+        private const int DWMWA_CAPTION_COLOR = 35;
+        private struct MARGINS { public int Left; public int Right; public int Top; public int Bottom; }
+
+
+
+        public Main()
+        {
+            // TESTING STUFF
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Padding = new Padding(1);
+            this.BackColor = Color.FromArgb(30, 30, 30); // Set a custom color for the title bar
+            MARGINS margins = new MARGINS { Left = 0, Right = 0, Top = 1, Bottom = 0 };
+            DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+
+            int color = ColorTranslator.ToWin32(this.BackColor);
+            DwmSetWindowAttribute(this.Handle, color, ref color, sizeof(int));
+
+
+            // end of test
+            UserSettings.Initiate();
+            Themes.Applied += ThemeApplied;
+            Themes.Initialize();
+            InitializeComponent();
+
+            this.Shown += new EventHandler(FormReady!);
+        }
+
+        // The function that will run when the Form is ready, and is shown.
+        private void FormReady(object sender, EventArgs e)
+        {
+            string CurrentTheme = UserSettings.GetValue("use_theme").ToString()!;
+            Themes.ApplyTheme(CurrentTheme);
+
+            // Check to make sure the user's path they set is still the SteamVR directory
+            string steamvr_path = Manager.steamvr_path;
+            if (!Manager.ValidatePath(Manager.steamvr_path))
+            {
+                System.Windows.Forms.MessageBox.Show("Please select where your SteamVR install is located!");
+                var (Success, Path) = Manager.AskForPath(false);
+                if (Success)
+                {
+                    steamvr_path = Path!;
+                    return;
+                }
+            }
+
+            // Separate if statement just in case the user chooses not to tell the program where SteamVR is located
+            if (Manager.ValidatePath(Manager.steamvr_path))
+            {
+                steamvr_directory.Text = steamvr_path;
+                steamvr_enabled.Checked = Manager.IsEnabled();
+                steamvr_toggle.Text = Manager.IsEnabled() ? "Disable SteamVR" : "Enable SteamVR";
+                return;
+            }
+            steamvr_directory.Text = "SteamVR not found!";
+        }
+
+        private void select_directory_Click(object sender, EventArgs e)
+        {
+            // Prompts the user once for their SteamVR directory, and update accordingly
+            var (Success, Path) = Manager.AskForPath(true);
+            if (Success)
+            {
+                steamvr_enabled.Checked = Manager.IsEnabled();
+                steamvr_toggle.Text = Manager.IsEnabled() ? "Disable SteamVR" : "Enable SteamVR";
+            }
+        }
+
+        // Changes the state if SteamVR is enabled or not
+        private void steamvr_toggle_Click(object sender, EventArgs e)
+        {
+            Manager.SetEnabled(!Manager.IsEnabled());
+            steamvr_enabled.Checked = Manager.IsEnabled();
+            steamvr_toggle.Text = Manager.IsEnabled() ? "Disable SteamVR" : "Enable SteamVR";
+        }
+
+        private void ThemeApplied(object sender, ThemeAppliedEventArgs e)
+        {
+            if (e.Theme != null)
+            {
+                XmlDocument Theme = e.Theme;
+                XmlNode MainWindow = Theme.SelectSingleNode("/Theme/Main")!;
+                if (MainWindow != null)
+                {
+
+                    XmlNode TitleBGC = MainWindow.SelectSingleNode("Titlebar_Color")!;
+                    Color title_color = ColorTranslator.FromHtml(TitleBGC.InnerText);
+                    int title_color_value = ColorTranslator.ToWin32(title_color);
+                    DwmSetWindowAttribute(this.Handle, DWMWA_CAPTION_COLOR, ref title_color_value, sizeof(int));
+
+
+                    XmlNode MainBGC = MainWindow.SelectSingleNode("Background_Color")!;
+                    this.BackColor = Themes.ParseHex(MainBGC.InnerText);
+                    XmlNode TopbarBGC = MainWindow.SelectSingleNode("Topbar_Background_Color")!;
+                    topbar.BackColor = Themes.ParseHex(TopbarBGC.InnerText);
+                    XmlNode FontColor = MainWindow.SelectSingleNode("Font_Color")!;
+                    steamvr_enabled_label.ForeColor = Themes.ParseHex(FontColor.InnerText);
+                    topbar.ForeColor = Themes.ParseHex(FontColor.InnerText);
+
+                    // temp
+                    steamvr_directory.BackColor = Themes.ParseHex(MainBGC.InnerText);
+                    steamvr_directory.ForeColor = Themes.ParseHex(FontColor.InnerText);
+
+                    select_directory.BackColor = Themes.ParseHex(TopbarBGC.InnerText);
+                    select_directory.ForeColor = Themes.ParseHex(FontColor.InnerText);
+
+                    steamvr_toggle.BackColor = Themes.ParseHex(TopbarBGC.InnerText);
+                    steamvr_toggle.ForeColor = Themes.ParseHex(FontColor.InnerText);
+
+                    
+                }
+            }
+        }
+    }
+}
+
